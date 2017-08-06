4c8f156ac8b242ad439d61b263d2df2934eda5b6
joaolpgomes <joao.lp.gomes@gmail.com>
2017-08-06 18:34:42 +0100

    Initial setup for react, webpack and babel

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>My Minimal React Webpack Babel Setup</div>,
+  document.getElementById('app')
+);
+
+module.hot.accept();
